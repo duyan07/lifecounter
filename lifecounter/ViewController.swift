b97d0b34fad6483939cc2cd3b02a62fc5041ebe6
nguyean <nguyean@uw.edu>
2025-04-22 23:24:41 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by An Nguyen on 4/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
